34de806a45edff8c3256a562e1005351020652f0
Federico De Faveri <defaveri@gmail.com>
2013-10-07 17:55:45 +0200

    Merged fix from brandstaetter pull request

--- yahoo-weather-java-api/src/main/java/com/github/fedy2/weather/binding/adapter/DateAdapter.java
+++ yahoo-weather-java-api/src/main/java/com/github/fedy2/weather/binding/adapter/DateAdapter.java
@@ -1,46 +1,45 @@
 /**
- * 
+ *
  */
 package com.github.fedy2.weather.binding.adapter;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * @author "Federico De Faveri defaveri@gmail.com"
- *
  */
 public class DateAdapter extends XmlAdapter<String, Date> {
 
-	protected Logger logger = LoggerFactory.getLogger(DateAdapter.class);
+    protected Logger logger = LoggerFactory.getLogger(DateAdapter.class);
 
-	protected SimpleDateFormat dateFormat = new SimpleDateFormat("d MMM yyyy");
+    protected SimpleDateFormat dateFormat = new SimpleDateFormat("d MMM yyyy", Locale.US);
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String marshal(Date v) throws Exception {
-		return dateFormat.format(v);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String marshal(Date v) throws Exception {
+        return dateFormat.format(v);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Date unmarshal(String v) throws Exception {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Date unmarshal(String v) throws Exception {
 
-		try {
-			return dateFormat.parse(v);
-		} catch(Exception e) {
-			logger.warn("Unknow date format \"{}\"",v);
-			return null;
-		}
-	}
+        try {
+            return dateFormat.parse(v);
+        } catch (Exception e) {
+            logger.warn("Unknown date format \"{}\"", v);
+            return null;
+        }
+    }
 
 }
--- yahoo-weather-java-api/src/main/java/com/github/fedy2/weather/binding/adapter/RFC822DateAdapter.java
+++ yahoo-weather-java-api/src/main/java/com/github/fedy2/weather/binding/adapter/RFC822DateAdapter.java
@@ -1,60 +1,59 @@
 /**
- * 
+ *
  */
 package com.github.fedy2.weather.binding.adapter;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * @author "Federico De Faveri defaveri@gmail.com"
- *
  */
 public class RFC822DateAdapter extends XmlAdapter<String, Date> {
 
-	public static final SimpleDateFormat rfc822DateFormats[] = new SimpleDateFormat[] {
-		new SimpleDateFormat("EEE, dd MMM yy HH:mm:ss z"),
-		new SimpleDateFormat("EEE, dd MMM yy HH:mm z"),
-		new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z"),
-		new SimpleDateFormat("EEE, dd MMM yyyy HH:mm z"),
-		new SimpleDateFormat("dd MMM yy HH:mm z"),
-		new SimpleDateFormat("dd MMM yy HH:mm:ss z"),
-		new SimpleDateFormat("dd MMM yyyy HH:mm z"),
-		new SimpleDateFormat("dd MMM yyyy HH:mm:ss z"),
-		//exception to RFC 822 format used by Yahoo "Thu, 22 Dec 2011 1:50 pm CET"
-		new SimpleDateFormat("EEE, dd MMM yyyy HH:mm a z")}; 
+    public static final SimpleDateFormat rfc822DateFormats[] = new SimpleDateFormat[]{
+            new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.US),
+            new SimpleDateFormat("EEE, d MMM yy HH:mm z", Locale.US),
+            new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.US),
+            new SimpleDateFormat("EEE, d MMM yyyy HH:mm z", Locale.US),
+            new SimpleDateFormat("d MMM yy HH:mm z", Locale.US),
+            new SimpleDateFormat("d MMM yy HH:mm:ss z", Locale.US),
+            new SimpleDateFormat("d MMM yyyy HH:mm z", Locale.US),
+            new SimpleDateFormat("d MMM yyyy HH:mm:ss z", Locale.US),
+            //exception to RFC 822 format used by Yahoo "Thu, 22 Dec 2011 1:50 pm CET"
+            new SimpleDateFormat("EEE, d MMM yyyy HH:mm a z", Locale.US)};
 
-	protected Logger logger = LoggerFactory.getLogger(RFC822DateAdapter.class);
-	
-	protected SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z");
+    protected Logger logger = LoggerFactory.getLogger(RFC822DateAdapter.class);
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String marshal(Date v) throws Exception {
-		return dateFormat.format(v);
-	}
+    protected SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z", Locale.US);
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Date unmarshal(String v) throws Exception {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String marshal(Date v) throws Exception {
+        return dateFormat.format(v);
+    }
 
-		for (SimpleDateFormat format:rfc822DateFormats) {
-			try {
-				return format.parse(v);
-			} catch(Exception e)
-			{}//skipping exception
-		}
-		logger.warn("Unknow date format \"{}\"",v);
-		return null;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Date unmarshal(String v) throws Exception {
+
+        for (SimpleDateFormat format : rfc822DateFormats) {
+            try {
+                return format.parse(v);
+            } catch (Exception e) {
+            }//skipping exception
+        }
+        logger.warn("Unknown date format \"{}\"", v);
+        return null;
+    }
 
 }
