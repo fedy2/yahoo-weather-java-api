f01bd75d74650a2023e2e5c951b57389a65e9a90
Philip Dakowitz <philip.dakowitz@dakowitz.de>
2016-10-13 13:56:36 +0200

    Added XML sample for places

--- src/test/java/com/github/fedy2/weather/test/TestJaxB.java
+++ src/test/java/com/github/fedy2/weather/test/TestJaxB.java
@@ -4,6 +4,7 @@
 package com.github.fedy2.weather.test;
 
 import com.github.fedy2.weather.data.Channel;
+import com.github.fedy2.weather.data.Place;
 import com.github.fedy2.weather.data.Rss;
 
 import javax.xml.bind.JAXBContext;
@@ -11,6 +12,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 
 /**
  * @author "Federico De Faveri defaveri@gmail.com"
@@ -35,6 +37,12 @@ public class TestJaxB {
 			Channel channel = (Channel) o;
 			System.out.println("Sunrise: " + channel.getAstronomy().getSunrise());
 			System.out.println("Sunset: " + channel.getAstronomy().getSunset());
+		}
+
+		rss = (Rss)unmarshaller.unmarshal(new FileReader("src/test/resources/xml/sample-place.xml"));
+		List<Place> places = rss.getResult();
+		for (Place place : places) {
+			System.out.println(place);
 		}
 	}
 
